e8cbde4b030b57ea2157ec5c18d5bd10d6717432
pierug <pierug11@o2.pl>
2017-04-27 16:37:36 +0200

    new matlab
    new matlab

--- matlabCourse/projekt3/segmentation.m
+++ matlabCourse/projekt3/segmentation.m
@@ -14,27 +14,46 @@ figure(3); clf;
 imhist(img2);
 
 % image binarisation, and remove particles touching border
-bin = img2 > 60;
+bin = img2 > 140;
 bin = imclearborder(bin, 18);
-imshow(bin);
+figure ,imshow(bin);
 
 % compute image labels, using minimal connectivity
 lbl = bwlabel(bin, 4);
 nLabels = max(lbl(:));
 
+stats = regionprops('table',bin,'Centroid','MajorAxisLength','MinorAxisLength');
+centers = stats.Centroid
+diameters = mean([stats.MajorAxisLength stats.MinorAxisLength],2);
+radii = diameters/2;
+fixedSize=0;
+for i=1:size(centers)
+    if diameters(i)>50
+        fixedSize=fixedSize+1;
+    end
+end
+
+fixedDiameters=zeros(fixedSize,1);
+fixedCenters=zeros(fixedSize,2);
+j=1;
+for i=1:size(centers)
+    if diameters(i)>50
+        fixedCenters(j,1) = centers(i,1);
+        fixedCenters(j,2) = centers(i,2);
+        fixedDiameters(j) = diameters(i);
+        j=j+1;
+    end
+end
+
+fixedradii = fixedDiameters/2;
 % display label image
 rgb = label2rgb(lbl, jet(nLabels), 'w', 'shuffle');
 figure(4); clf;
-imshow(rgb);
 
+RGB = insertText(rgb,fixedCenters,fixedDiameters,'FontSize',25,'AnchorPoint','LeftBottom');
+imshow(RGB);
 
+hold on
+viscircles(fixedCenters,fixedradii);
+hold off
 
-% compute diameters with corresponding angles
-[diams thetas] = imMaxFeretDiameter(lbl);
-
-% also compute centroids
-centros = imCentroid(lbl);
-
-% overlay result on image
-hold on;
-drawCenteredEdge([centros diams thetas], 'linewidth', 2, 'color', 'k');
