79e89fe8c7e54e1c7e78ce0d909440048b5ebb19
Adam Piasecki <adam.piasecki@pollub.edu.pl>
2017-07-05 21:16:57 +0200

    First commint

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: adam
+ *
+ * Created on 5 lipca 2017, 21:03
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv) {
+    string s = "Elo '" "Elo ";
+    cout<<s<<endl;
+    return 0;
+}
+
