fea5b3f2d5d21b043b1938cb62b3721f3327e723
Adam Piasecki <adam.piasecki@pollub.edu.pl>
2017-07-05 22:21:15 +0200

    new Onlyy one

---
+++ kkk.h
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   kkk.h
+ * Author: adam
+ *
+ * Created on 5 lipca 2017, 22:17
+ */
+
+#ifndef KKK_H
+#define KKK_H
+
+int gggg(){
+    return 7;
+}
+
+#endif /* KKK_H */
+
