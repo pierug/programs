08c00d9ab0fea2637e69d13595cc3b2ef87d4807
Adam Piasecki <adam.piasecki@pollub.edu.pl>
2017-07-05 22:25:56 +0200

    new Onlyy one

---
+++ Neaea.h
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Neaea.h
+ * Author: adam
+ *
+ * Created on 5 lipca 2017, 22:05
+ */
+
+#ifndef NEAEA_H
+#define NEAEA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    int intint(){
+        return 2;
+    }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NEAEA_H */
+
